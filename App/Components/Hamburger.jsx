d151f1c6d245edbfd54916f98deb41ac66f0221a
petergiromagny <peter.giromagny@viacesi.fr>
2021-04-01 21:37:14 +0200

    Create hamburger components

---
+++ App/Components/Hamburger.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import colors from '../Constants/colors';
+
+const styles = StyleSheet.create({
+  line: {
+    height: 4,
+    borderRadius: 50,
+    marginVertical: 3,
+    backgroundColor: colors.dark,
+  },
+  top: { width: 15 },
+  middle: { width: 30 },
+  bottom: { width: 20 },
+});
+
+/**
+ * @todo: Create animation on hamburger (wave)
+ */
+
+export default () => (
+  <>
+    <View style={[styles.line, styles.top]} />
+    <View style={[styles.line, styles.middle]} />
+    <View style={[styles.line, styles.bottom]} />
+  </>
+);
