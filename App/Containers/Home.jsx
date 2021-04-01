0eb308971de58a95a5941b21c36bcbc2311b4f91
petergiromagny <peter.giromagny@viacesi.fr>
2021-04-01 21:35:15 +0200

    Update folder name to 'Containers'

--- App/Containers/Home.jsx
+++ App/Containers/Home.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Avatar from '../Components/Avatar';
 import colors from '../Constants/colors';
 
 const styles = StyleSheet.create({
@@ -9,23 +10,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.background,
   },
-  text: {
-    fontFamily: 'PoppinsMedium',
-  },
+  text: { fontFamily: 'PoppinsMedium' },
 });
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // Add State
+      /** Add State */
     };
   }
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.text}>This is the Home Page</Text>
+        <Avatar />
       </SafeAreaView>
     );
   }
