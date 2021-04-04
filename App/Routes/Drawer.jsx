41783d775dde3826bd0d92a0155fb1d8aaef6bd9
petergiromagny <peter.giromagny@viacesi.fr>
2021-04-04 11:51:27 +0200

    Add drawerIcon

--- App/Routes/Drawer.jsx
+++ App/Routes/Drawer.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  Entypo,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from '@expo/vector-icons';
 import { HomeStack, MealsStack, RecipesStack, SettingsStack } from './Stack';
 
 import colors from '../Constants/colors';
@@ -14,20 +19,74 @@ const DrawerRoutes = () => (
     drawerType='slide'
     drawerStyle={{ backgroundColor: colors.background }}
     drawerContentOptions={{
-      activeTintColor: colors.green,
+      activeTintColor: colors.dark,
       inactiveTintColor: colors.green,
       itemStyle: { marginVertical: 5 },
       labelStyle: {
         fontFamily: 'PoppinsBold',
         fontSize: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
       },
     }}
     drawerContent={(props) => <DrawerContent {...props} />}
   >
-    <Drawer.Screen name='Home' component={HomeStack} />
-    <Drawer.Screen name='Recipes' component={RecipesStack} />
-    <Drawer.Screen name='Meals' component={MealsStack} />
-    <Drawer.Screen name='Settings' component={SettingsStack} />
+    <Drawer.Screen
+      name='Home'
+      component={HomeStack}
+      options={{
+        title: 'Home',
+        drawerIcon: ({ size, focused }) => (
+          <Entypo
+            name='home'
+            size={size}
+            color={focused ? colors.dark : colors.green}
+          />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name='Recipes'
+      component={RecipesStack}
+      options={{
+        title: 'Recipes',
+        drawerIcon: ({ size, focused }) => (
+          <MaterialCommunityIcons
+            name='food-variant'
+            size={size}
+            color={focused ? colors.dark : colors.green}
+          />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name='Meals'
+      component={MealsStack}
+      options={{
+        title: 'Last Meals',
+        drawerIcon: ({ size, focused }) => (
+          <MaterialCommunityIcons
+            name='bowl-mix'
+            size={size}
+            color={focused ? colors.dark : colors.green}
+          />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name='Settings'
+      component={SettingsStack}
+      options={{
+        title: 'Settings',
+        drawerIcon: ({ size, focused }) => (
+          <MaterialIcons
+            name='settings'
+            size={size}
+            color={focused ? colors.dark : colors.green}
+          />
+        ),
+      }}
+    />
   </Drawer.Navigator>
 );
 
