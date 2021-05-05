44b709c34af9760fdbcdfa6862d26be22fd5378d
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-05-05 12:40:56 +0200

    Add loading Lottie Files component animation

--- App/Components/ScanningAnimation.jsx
+++ App/Components/ScanningAnimation.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import Svg, { G, Circle } from 'react-native-svg';
+import LottieView from 'lottie-react-native';
+import * as animationLoading from '../Assets/Animations/loading.json';
 
 import colors from '../Constants/colors';
 
@@ -22,47 +23,14 @@ const styles = StyleSheet.create({
 export default function ScanningAnimation(props) {
   const { radius } = props;
 
-  const circleRef = useRef();
-
-  const strokeWidth = 10;
-  const halfCircle = radius + strokeWidth;
-  // const circumference = 2 * Math.PI * radius;
-  const dashArray = 125;
-  const color = colors.orange;
-
   return (
     <View style={styles.container}>
       <View style={{ width: radius * 2, height: radius * 2 }}>
-        <Svg
-          height={radius * 2}
-          width={radius * 2}
-          viewBox={`0 0 ${halfCircle * 2} ${halfCircle * 2}`}
-        >
-          <G rotation='-90' origin={`${halfCircle}, ${halfCircle}`}>
-            <Circle
-              ref={circleRef}
-              cx='50%'
-              cy='50%'
-              r={radius}
-              fill='transparent'
-              stroke={color}
-              strokeWidth={strokeWidth}
-              strokeLinecap='round'
-              strokeDashoffset={220}
-              strokeDasharray={dashArray}
-            />
-            <Circle
-              cx='50%'
-              cy='50%'
-              r={radius}
-              fill='transparent'
-              stroke={color}
-              strokeWidth={strokeWidth}
-              strokeLinejoin='round'
-              strokeOpacity='.1'
-            />
-          </G>
-        </Svg>
+        <LottieView
+          source={animationLoading}
+          style={StyleSheet.absoluteFillObject}
+          autoPlay
+        />
         <View style={[StyleSheet.absoluteFillObject, styles.text]}>
           <Text style={styles.text}>Scanning</Text>
           <Text style={styles.text}>your meal</Text>
