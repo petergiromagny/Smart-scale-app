bee16a5e664166a2ccc1ae83df746683323749b7
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-05-05 12:39:57 +0200

    Create donut animation

--- App/Components/Donut.jsx
+++ App/Components/Donut.jsx
@@ -1,22 +1,73 @@
-import React from 'react';
-import { StyleSheet, View, Text, Platform } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Animated,
+  Easing,
+  TextInput,
+} from 'react-native';
 import Svg, { Circle, G } from 'react-native-svg';
 
 const styles = StyleSheet.create({
-  textContainer: {},
   text: {
     fontFamily: 'PoppinsMedium',
     textAlign: 'center',
   },
 });
 
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
 export default function Donut(props) {
-  const { kcal, objKcal, color, radius, strokeWidth } = props;
+  const { kcal, objKcal, color, radius, strokeWidth, donutAnimated } = props;
   const circumference = 2 * Math.PI * radius;
   const halfCircle = radius + strokeWidth;
-  const maxKcal = (100 * kcal) / objKcal;
 
-  const strokeDashoffset = circumference - (circumference * maxKcal) / 100;
+  // Init ref target component for animation
+  const animated = useRef(new Animated.Value(0)).current;
+  const circleRef = useRef();
+  const inputRef = useRef();
+
+  // Create animation
+  const animation = (toValue, duration, delay) =>
+    Animated.timing(animated, {
+      toValue,
+      duration,
+      delay,
+      useNativeDriver: true,
+      easing: Easing.out(Easing.ease),
+    }).start();
+
+  // Active animation when screen is loaded
+  useEffect(() => {
+    if (donutAnimated) {
+      animation(kcal, 1000, 250);
+    } else {
+      animation(kcal, 0, 0);
+    }
+    animated.addListener(
+      (v) => {
+        const maxKcal = (100 * v.value) / objKcal;
+        const strokeDashoffset =
+          circumference - (circumference * maxKcal) / 100;
+        if (inputRef?.current) {
+          inputRef.current.setNativeProps({
+            text: `${Math.round(v.value)}`,
+          });
+        }
+        if (circleRef?.current) {
+          circleRef.current.setNativeProps({
+            strokeDashoffset,
+          });
+        }
+      },
+      [objKcal, kcal]
+    );
+    return () => {
+      animated.removeAllListeners();
+    };
+  });
 
   return (
     <View
@@ -39,20 +90,21 @@ export default function Donut(props) {
             stroke={color}
             fill='transparent'
             strokeWidth={strokeWidth}
-            strokeLinecap='round'
-            strokeDashoffset={strokeDashoffset}
-            strokeDasharray={circumference}
+            strokeLinejoin='round'
+            strokeOpacity='.1'
           />
 
-          <Circle
+          <AnimatedCircle
+            ref={circleRef}
             cx='50%'
             cy='50%'
             r={radius}
             stroke={color}
             fill='transparent'
             strokeWidth={strokeWidth}
-            strokeLinejoin='round'
-            strokeOpacity='.1'
+            strokeLinecap='round'
+            strokeDashoffset={circumference}
+            strokeDasharray={circumference}
           />
         </G>
       </Svg>
@@ -62,18 +114,16 @@ export default function Donut(props) {
           {
             top: radius - radius / 2.8,
             left: halfCircle - radius - strokeWidth,
-            ...Platform.select({
-              ios: {
-                transform: [{ translateY: 5 }],
-              },
-            }),
           },
-          styles.textContainer,
         ]}
       >
-        <Text style={[{ fontSize: radius / 2.8, color }, styles.text]}>
-          {kcal}
-        </Text>
+        <AnimatedTextInput
+          ref={inputRef}
+          underlineColorAndroid='transparent'
+          editable={false}
+          defaultValue='0'
+          style={[{ fontSize: radius / 2.8, color }, styles.text]}
+        />
         <Text style={[{ fontSize: radius / 2.8, color }, styles.text]}>
           kcal
         </Text>
